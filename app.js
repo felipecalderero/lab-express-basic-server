cac1a1c7afd461f5d9858066fed68114c4f3a0e5
felipecalderero <felipecalderero@gmail.com>
2024-04-22 13:35:12 +0200

    solved with AI help... somer parts missing to be able to fully run the server

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 // IMPORT PACKAGES
-// Here you should import the required packages for your Express app: `express` and `morgan`
-
-
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs"); // File system module to read files
 
 // CREATE EXPRESS APP
-// Here you should create your Express app:
-
-
+const app = express();
 
 // MIDDLEWARE
-// Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
-
-
+app.use(morgan("dev")); // Log all incoming requests
+app.use(express.static("public")); // Serve static files from 'public' folder
+app.use(express.json()); // Parse incoming JSON payloads
 
 // ROUTES
-// Start defining your routes here:
-
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+app.get("/api/projects", (req, res) => {
+  // Read the projects.json file from the data directory and parse it to JSON
+  fs.readFile(__dirname + "/data/projects.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading the projects file.");
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+app.get("/api/articles", (req, res) => {
+  // Read the articles.json file from the data directory and parse it to JSON
+  fs.readFile(__dirname + "/data/articles.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading the articles file.");
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+// 404 error handling
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/views/not-found.html");
+});
 
 // START THE SERVER
-// Make your Express server listen on port 5005:
+app.listen(5005, () => console.log("My first app listening on port 5005!"));
